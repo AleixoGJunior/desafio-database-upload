63101e4fcdd0d83ee3f0e90e32e5b8baa277ecef
Aleixo G Junior <aleixo.cco@gmail.com>
2020-04-15 19:57:27 -0300

    Finalizado service de importação de arquivo CSV. Não estou contente com o cõdigo, espero encontrar uma forma alternativa pra resolução.

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -10,30 +10,54 @@ interface Request {
   filename: string;
 }
 
+interface CSVTransactions {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
+
 class ImportTransactionsService {
   public async execute({ filename }: Request): Promise<Transaction[]> {
     const filePath = path.join(uploadConfig.directory, filename);
-
-    const transactions: Transaction[] = [];
     const createTransaction = new CreateTransactionService();
 
-    const fileStream = fs.createReadStream(filePath);
-    const csvStream = csvParse({ delimiter: ',', columns: true, trim: true })
-      .on('data', async data => {
-        const [title, type, value, category] = data;
-        const transaction = await createTransaction.execute({
-          title,
-          type,
-          value,
-          category,
-        });
-        transactions.push(transaction);
-      })
-      .on('end', () => {
-        fs.promises.unlink(filePath);
-      });
+    const parsedTransactions = await new Promise<CSVTransactions[]>(
+      (resolve, reject) => {
+        const csvTransactions: CSVTransactions[] = [];
+        fs.createReadStream(filePath)
+          .pipe(csvParse({ delimiter: ',', columns: true, trim: true }))
+          .on('data', async data => {
+            csvTransactions.push(data);
+          })
+          .on('error', () => reject)
+          .on('end', () => {
+            resolve(csvTransactions);
+            fs.promises.unlink(filePath);
+          });
+      },
+    );
 
-    fileStream.pipe(csvStream);
+    async function importTransactions(
+      toImportTransactions: Array<CSVTransactions>,
+    ): Promise<Transaction[]> {
+      const importedTransactions: Transaction[] = [];
+
+      /* eslint-disable no-restricted-syntax */
+      for await (const data of toImportTransactions) {
+        const transaction = await createTransaction.execute({
+          title: data.title,
+          type: data.type,
+          value: data.value,
+          category: data.category,
+        });
+        importedTransactions.push(transaction);
+      }
+
+      return importedTransactions;
+    }
+
+    const transactions = await importTransactions(parsedTransactions);
 
     return transactions;
   }
