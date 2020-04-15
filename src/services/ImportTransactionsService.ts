23abb2ccfd0c9d942998d624be3546aa0c55ed43
Aleixo G Junior <aleixo.cco@gmail.com>
2020-04-15 16:56:52 -0300

    Implementado integrações com o banco de dados, rotas e services. Service de importação ainda falhando no teste

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,41 @@
+import csvParse from 'csv-parse';
+import path from 'path';
+import fs from 'fs';
+import uploadConfig from '../config/upload';
+
 import Transaction from '../models/Transaction';
+import CreateTransactionService from './CreateTransactionService';
+
+interface Request {
+  filename: string;
+}
 
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  public async execute({ filename }: Request): Promise<Transaction[]> {
+    const filePath = path.join(uploadConfig.directory, filename);
+
+    const transactions: Transaction[] = [];
+    const createTransaction = new CreateTransactionService();
+
+    const fileStream = fs.createReadStream(filePath);
+    const csvStream = csvParse({ delimiter: ',', columns: true, trim: true })
+      .on('data', async data => {
+        const [title, type, value, category] = data;
+        const transaction = await createTransaction.execute({
+          title,
+          type,
+          value,
+          category,
+        });
+        transactions.push(transaction);
+      })
+      .on('end', () => {
+        fs.promises.unlink(filePath);
+      });
+
+    fileStream.pipe(csvStream);
+
+    return transactions;
   }
 }
 
